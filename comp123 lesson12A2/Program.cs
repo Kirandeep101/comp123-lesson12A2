a8a663bc154f5a6a5d62f9756c0d775e5f532f09
300915966 <300915966@PR-A315-17>
2017-08-03 10:44:32 -0400

    created form

--- comp123 lesson12A2/Program.cs
+++ comp123 lesson12A2/Program.cs
@@ -16,7 +16,7 @@ namespace comp123_lesson12A2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Calculator());
         }
     }
 }
